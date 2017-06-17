453ea4acce784afd4f1d53c3a8ebe1864af7b440
Depak Aggarwal <daggarwal77@gmail.com>
2017-06-17 11:47:19 +0530

    Rat maze comments added

--- 16-Jun-03/rat_maze1.cpp
+++ 16-Jun-03/rat_maze1.cpp
@@ -3,6 +3,7 @@ int M, N;
 using namespace std;
 int path[10][10];   //saves the path
 
+//prints 2D char array
 void printChar(char board[][10]){
     for(int i = 0; i < M; ++i){
         for(int j = 0; j < N; ++j) cout << board[i][j] << " ";
@@ -10,7 +11,7 @@ void printChar(char board[][10]){
     }
 }
 
-
+//prints 2D int array
 void printInt(int board[][10]){
     for(int i = 0; i < M; ++i){
         for(int j = 0; j < N; ++j) cout << board[i][j] << " ";
@@ -18,6 +19,8 @@ void printInt(int board[][10]){
     }
 }
 
+//sol finds one of the possible paths and sets the path[][] 2D array 
+//with that path
 bool sol(char board[][10], int x, int y){
     //if I am goal
     if (x == M - 1 && y == N - 1){
@@ -25,30 +28,37 @@ bool sol(char board[][10], int x, int y){
         return true;
     }
 
+    //Oops! I have reached a bomb following the path I am currently following
     if (board[x][y] == 'X') return false;
 
+    //Lets "say" the x,y box CAN be a part of the solution
     path[x][y] = 1;
 
+    //If I am inside the matrix, I ll move in forward
     if (y < N){
         if (sol(board, x, y + 1) == true){
             return true;
         }
     }
 
+    //If I am inside the matrix, I ll move downwards
     if (x < M){
         if (sol(board, x + 1, y) == true){
             return true;
         }
     }
 
-    path[x][y] = 0;
+    //Oops! I can't find a way moving in forward AND downward direction
+    //x,y cell CANNOT form part of the path
+    path[x][y] = 0;     //restoring the previous state of the path[][]
+
     return false;
 }
 
 
 int main(){
 	char maze[][10] = {
-		"0X00XX",
+		"0000XX",
         "X0XX0X",
         "X000X0",
         "00X000"
